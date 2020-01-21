51624bd0f0aa1f71ca2dc0c5aa9ccb50644a8077
David Barney <davidjbarney@gmail.com>
2020-01-21 13:55:48 -0500

    Functionized queries and data reshaping

---
+++ 2_scripts/query_flat.R
@@ -0,0 +1,97 @@
+#' spotifyr::search_spotify() wrapper
+#'
+#' @param artists A string vector of artist names to search
+#' @param authorization Spotify API token as required by `spotifyr`
+#'
+#' @return A dataframe of each artist name and Spotify uri
+#' @export
+#'
+#' @examples
+query_artists_flat <- function(artists, authorization = get_spotify_access_token()){
+  if (!is.character(artists)){
+    stop("'artists' must be a character vector of artist names", call. = TRUE)
+  }
+  artist_list <- list()
+  for (i in 1:length(artists)){
+    temp <- search_spotify(artists[[i]], type = "artist", limit = 1)
+    Sys.sleep(0.25)
+    temp$genres[[1]] <- str_c(temp$genres[[1]], collapse = ", ")
+    artist_list[[i]] <- temp
+  }
+  artists_df <- do.call(rbind, artist_list)
+  return(artists_df)
+}
+
+#' spotifyr::get_artist_albums() wrapper
+#'
+#' @param artist_uris A string vector of artist uri identifiers to query
+#' @param authorization Spotify API token as required by `spotifyr`
+#'
+#' @return A dataframe of albums for each artist
+#' @export
+#'
+#' @examples
+query_albums_flat <- function(artist_uris, authorization = get_spotify_access_token()){
+  if (!is.character(artist_uris)){
+    stop("'artist_uris' must be a character vector of artist Spotify uri identifiers", call. = TRUE)
+  }
+  album_list <- list()
+  for(i in 1:length(artist_uris)){
+    temp <- get_artist_albums(artist_uris[[i]], 
+                              include_groups = c("album", "single"), 
+                              market = "US")
+    Sys.sleep(0.25) # Pause to avoid breaking the API calls
+    temp$artist <- NA
+    for (j in 1:nrow(temp)){
+      temp$artist[[j]] <- str_c(temp[j,3][[1]][["name"]], collapse = ", ")
+    }
+    album_list[[i]] <- temp
+  }
+  albums_df <- do.call(rbind, album_list)
+  return(albums_df)
+}
+
+#' spotifyr::get_album_tracks and spotifyr::get_track wrapper
+#'
+#' @param album_uris A string vector of album uri identifiers to query
+#' @param albu,_names A string vector of album names associated with each album uri
+#' @param authorization Spotify API token as required by `spotifyr`
+#'
+#' @return
+#' @export
+#'
+#' @examples
+query_tracks_flat <- function(album_uris, album_names, authorization = get_spotify_access_token()){
+  if (!is.character(album_uris)){
+    stop("'album_uris' must be a character vector of album Spotify uri identifiers", call. = TRUE)
+  }
+  if (!is.character(album_names)){
+    stop("'album_names' must be a character vector of album names associated with 'album_uris'", call. = TRUE)
+  }
+  if (length(album_uris) != length(album_names)){
+    stop("'album_names' and 'album_uris' must be character vectors of the same length'", call. = TRUE)
+  }
+  tracklists_list <- list()
+  for (i in 1:length(album_uris)){
+    df_temp <- get_album_tracks(album_uris[[i]])
+    Sys.sleep(0.25) # Pause to avoid breaking the API calls
+    df_temp$track_artists <- NA
+    df_temp$album_name <- NA
+    df_temp$track_popularity <- NA
+    for (j in 1:nrow(df_temp)){
+      df_temp$album_name <- album_names[[i]]
+      list_temp <- get_track(df_temp$id[[j]])
+      Sys.sleep(0.25)
+      df_temp$track_popularity[[j]] <- list_temp[["popularity"]]
+      df_temp$track_artists[[j]] <- str_c(list_temp[["artists"]][["name"]], collapse = ", ")
+    }
+    tracklists_list[[i]] <- df_temp
+  }
+  tracklists_df <- do.call(rbind, tracklists_list)
+  return(tracklists_df)
+}
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Overview
-This project maps the structure of artist features in popular hip hop with network science methodology, specifically motivated by sociological approaches to social network analysis.
+This project maps the social network structure of popular hip hop artists through track features.
 
 Data is collected via the [Spotify API](https://developer.spotify.com/documentation/web-api/) using the [`spotifyr`](https://github.com/charlie86/spotifyr) library in [R](https://www.r-project.org/). 
 
-As a work in progress, this project is currently organized as a notebook (`rap-feature-network-notebook.Rmd` in the `2_scripts` subdirectory).
+As a work in progress, this project is largely organized as a notebook (`rap-feature-network-notebook.Rmd` in the `2_scripts` subdirectory).
 
 # Disclaimer
-This repo contains a non-profit personal project that utilizes the Spotify API in accordance with the [Developer Terms of Service](https://developer.spotify.com/terms/).
+This is a personal project, and definitely not-for-profit. Code and data in this repo comply with the [Developer Terms of Service](https://developer.spotify.com/terms/) for Spotify's API.
